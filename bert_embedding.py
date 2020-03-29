061adf7ab5ad60a370fa02b61ec8d40e3ab17309
swati2494 <swati.choudhary.24@gmail.com>
2020-03-29 13:39:12 +0300

    Added glove embedding for 4 dimensions

--- bert_embedding.py
+++ bert_embedding.py
@@ -9,9 +9,14 @@ from tensorflow.keras.models import Model
 from tensorflow.keras.callbacks import ModelCheckpoint
 import tensorflow_hub as hub
 
+"these check if gpu is available or not"
+#from tensorflow.python.client import device_lib
+#print(device_lib.list_local_devices())
+
+#bert tokenization file
 import tokenization
 
-
+#prepare bert embedding format for dataset
 def bert_encode(texts, tokenizer, max_len=512):
     all_tokens = []
     all_masks = []
@@ -19,15 +24,29 @@ def bert_encode(texts, tokenizer, max_len=512):
 
     for text in texts:
         text = tokenizer.tokenize(text)
-
+        # subtract 2 as cls ans sep tags to be added
         text = text[:max_len - 2]
+        # get text to comply with bert embedding format
         input_sequence = ["[CLS]"] + text + ["[SEP]"]
+        #print("Input: ", input_sequence)
         pad_len = max_len - len(input_sequence)
+        #print("Input pad len :", pad_len)
 
+        # assigns each word a token number and converts text to token ids
         tokens = tokenizer.convert_tokens_to_ids(input_sequence)
+        #print("Token ids : ", tokens)
+
+        # padding till 160 length where 0 is the default
         tokens += [0] * pad_len
+        #print("Tokens after padding : ", tokens)
+
+        # padding 1 and 0 where 1 tells token present
         pad_masks = [1] * len(input_sequence) + [0] * pad_len
+        #print("Pad masks :", pad_masks)
+
+        # array of only 0's
         segment_ids = [0] * max_len
+        #print("Segments : ", segment_ids)
 
         all_tokens.append(tokens)
         all_masks.append(pad_masks)
@@ -37,14 +56,21 @@ def bert_encode(texts, tokenizer, max_len=512):
 
 
 def build_model(bert_layer, max_len=512):
-    input_word_ids = Input(
-        shape=(max_len,), dtype=tf.int32, name="input_word_ids")
+
+    input_word_ids = Input(shape=(max_len,), dtype=tf.int32, name="input_word_ids")
     input_mask = Input(shape=(max_len,), dtype=tf.int32, name="input_mask")
+    #print("Input mask : ", input_mask)
+
     segment_ids = Input(shape=(max_len,), dtype=tf.int32, name="segment_ids")
+    #print("segments : ", segment_ids)
 
     _, sequence_output = bert_layer([input_word_ids, input_mask, segment_ids])
+    #print("Seq Output: ", sequence_output)
+
     clf_output = sequence_output[:, 0, :]
+    #print("CLF output: ", clf_output)
     out = Dense(1, activation='sigmoid')(clf_output)
+    #print("After activation: ", out)
 
     model = Model(inputs=[input_word_ids, input_mask,
                           segment_ids], outputs=out)
@@ -54,6 +80,10 @@ def build_model(bert_layer, max_len=512):
     return model
 
 
+#load downloaded module
+#module_url = "1"
+
+#train bert embeddings
 module_url = "https://tfhub.dev/tensorflow/bert_en_uncased_L-24_H-1024_A-16/1"
 bert_layer = hub.KerasLayer(module_url, trainable=True)
 
@@ -73,9 +103,9 @@ model.summary()
 
 train_history = model.fit(
     train_input, train_labels,
-    validation_split=0.2,
-    epochs=3,
-    batch_size=16
+    validation_split=0.1,
+    epochs=1,
+    batch_size=4
 )
 
 model.save('model.h5')
